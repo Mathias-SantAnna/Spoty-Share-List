beb52511042353e49de2fafa3eb0a49603547057
Mathias-SantAnna <dark_tricolor@hotmail.com>
2021-05-24 05:17:47 +0000

    Add_playlist created and working

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
-
 
 
 app = Flask(__name__)
@@ -24,9 +22,10 @@ mongo = PyMongo(app)
 @app.route("/get_playlists")
 def get_playlists():
     playlists = mongo.db.playlists.find()
-    return render_template("playlists.html", playlists=playlists)
+    return render_template("/playlists.html", playlists=playlists)
 
 
+# REGISTER
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -49,9 +48,10 @@ def register():
         flash("Registration Successful!")
         return redirect(url_for("profile", username=session["user"]))
         
-    return render_template("register.html")
+    return render_template("/register.html")
 
 
+# LOGIN
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -78,29 +78,59 @@ def login():
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
 
-    return render_template("login.html")
+    return render_template("/login.html")
 
 
+# PROFILE PAGE
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-
     if session["user"]:
         return render_template("profile.html", username=username)
 
     return redirect(url_for("login"))
 
 
+# LOGOUT
 @app.route("/logout")
 def logout():
     # remove user from session cookie
     flash("You have been logged out")
-    session.pop("user")
+    session.clear()
     return redirect(url_for("login"))
 
 
+# ADD PLAYLIST
+@ app.route("/add_playlist", methods=["GET", "POST"])
+def add_playlist():
+    if not session.get("user"):
+        render_template("templates/error_handlers/404.html")
+
+    if request.method == "POST":
+        Playlist = {
+            "genre_name": request.form.get("genre_name"),
+            "playlist_name": request.form.get("playlist_name"),
+            "img_url": request.form.get("img_url"),
+            "playlist_details": request.form.get("playlist_details"),
+            "playlist_tracks": request.form.get("playlist_tracks"),
+            "artist_name": request.form.get("artist_name"),
+            "created_by": session["user"],
+            "playlist_url": request.form.get("playlist_url")
+        }
+
+        mongo.db.playlist.insert_one(Playlist)
+        flash("Playlist successfully added")
+        return redirect(url_for("profile", username=session['user']))
+
+    music_genre = mongo.db.music_genre.find().sort("genre_name", 1)
+    return render_template(
+        "playlists/add_playlist.html", music_genre=music_genre
+        )
+
+
+# THE APP
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
