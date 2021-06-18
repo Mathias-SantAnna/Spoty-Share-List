f25e26a9f32874d633e8d560237e0d63b8764153
Mathias-SantAnna <dark_tricolor@hotmail.com>
2021-06-18 04:14:35 +0000

    small changes to app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 # Import Python Modules
 import os
 import re
-import pymongo
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from flask_pymongo import ObjectId
-from pymongo import MongoClient
 import urllib.parse
 from werkzeug.security import generate_password_hash, check_password_hash
 
